affab3d0409661bf857aaec2210afe98b67a6e45
matsuba0721 <matsuba0721.ggl@gmail.com>
2021-09-26 15:21:00 +0900

    fix get startrd

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
-function accountChanged(account) {}
+function accountChanged(account) {
+    if (started && account.id > 0) {
+        window.location.href = "sheet";
+    }
+}
 function toProfileCard(id, profile) {
     var list = `<div class="ui large inverted horizontal list meta"><div class="item"><div class="content"><div class="header">職業</div><div class="description">${profile.job}</div></div></div><div class="item"><div class="content"><div class="header">年齢</div><div class="ui center aligned description">${profile.age}</div></div></div><div class="item"><div class="content"><div class="header">性別</div><div class="ui center aligned description">${profile.gender}</div></div></div></div>`;
     var content = `<div class="content"><img id="profile-image-${id}" class="left floated tiny ui image" src="images/loading.gif" /><div class="header">${profile.name}</div><div class="meta">${toTags(profile.tag).join(",")}</div>${list}</div>`;
@@ -21,13 +25,20 @@ function linkView(e) {
 account = getLoginAccount();
 
 window.onload = function () {
-    $("#link-sheet")[0].addEventListener("click", function (e) {
-        window.location.href = "sheet";
+    $("#start")[0].addEventListener("click", function (e) {
+        if (account.id > 0) {
+            window.location.href = "sheet";
+        } else {
+            started = true;
+            $(".ui.account-sign-up").hide();
+            $(".ui.account-sign-in").show();
+            $(".ui.account.modal").modal("show", { duration: 200 });
+        }
     });
 
     initSigns();
     initAccount(account);
-    if(localStorage.index_investigators){
+    if (localStorage.index_investigators) {
         $("#investigators").append(localStorage.index_investigators);
     }
 
@@ -39,8 +50,8 @@ window.onload = function () {
             $("#investigator-" + investigator.id + "-view")[0].addEventListener("click", linkView);
         }
         localStorage.index_investigators = $("#investigators")[0].innerHTML;
-        
-        setTimeout(function(){
+
+        setTimeout(function () {
             for (var i = 0; i < investigators.length; i++) {
                 var investigator = investigators[i];
                 $(`#profile-image-${investigator.id}`)[0].src = `img?v=${investigator.id}`;
