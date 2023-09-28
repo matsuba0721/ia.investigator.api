6e481f19e5fe6a8ea2085a2991ae98286ee3b714
matsuba0721 <matsuba0721.ggl@gmail.com>
2023-09-28 23:25:44 +0900

    secret key

--- public/js/view.js
+++ public/js/view.js
@@ -2,8 +2,14 @@ function accountChanged(account) {
     getInvestigatorEditable(account, parseInt(getParam("v")), function (editable) {
         if (editable) {
             $("#investigator-edit").prop("disabled", false);
+            $(".secret-data").each(function (index, element) {
+                element.style.pointerEvents = 'all'
+            });
         } else {
             $("#investigator-edit").prop("disabled", true);
+            $(".secret-data").each(function (index, element) {
+                element.style.pointerEvents = 'none'
+            });
         }
     });
 }
@@ -13,6 +19,15 @@ function initInvestigator(investigator) {
     }else{
         document.title = "新しい探索者 | R'lyeh House";
     }
+
+    sha256(investigator.id.toString()).then((hashedKey) => {
+        if(hashedKey == getParam("key") || !investigator.isHidden){
+            $(".secret-data").each(function (index, element) {
+                element.style.pointerEvents = 'all'
+            });
+        }
+    });
+
     var cthulhuSkill = firstOrDefault(function (e) {
         return e.name == "クトゥルフ神話";
     }, investigator.skills);
